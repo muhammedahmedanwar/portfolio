944afb5677d050bb42fedec476bf2ed644d54564
MuhammedAhmedAnwar <101336913+muhammedahmedanwar@users.noreply.github.com>
2023-09-21 02:46:10 +0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "./header.css";
-import logo from '../../assets/Jimmyz-1.jpg'
+import logo from '../../assets/png16.png'
 
 const Header = () => {
     window.addEventListener("scroll", function(){
