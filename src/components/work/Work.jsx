1f9399ac2b5cff3babfe457a74438e6caad77377
MuhammedAhmedAnwar <101336913+muhammedahmedanwar@users.noreply.github.com>
2023-09-20 19:31:27 +0300

    Update Work.jsx

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -7,6 +7,7 @@ const Work = () => {
     <section className="work section" id="portfolio">
         <h2 className="section__title">Portfolio</h2>
         <span className="section__subtitle">Most recent works</span>
+        <span className="section__subtitle">I have successfully completed various backend projects. If you'd like to see my work, please don't hesitate to get in touch with me.</span>
         <Works />
     </section>
   )
