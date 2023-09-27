c891e38e1fef696233c2754450a8a7f3bd8c73f1
MuhammedAhmedAnwar <101336913+muhammedahmedanwar@users.noreply.github.com>
2023-09-27 05:49:30 +0300

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/muhammedahmedanwar/" className="home__social-icon" target="_blank"><i className="uil uil-instagram"></i></a>
+        <a href="https://www.instagram.com/jimmyz_wd/" className="home__social-icon" target="_blank"><i className="uil uil-instagram"></i></a>
         <a href="https://www.linkedin.com/in/muhammed-ahmed-anwar-8a5433213/" className="home__social-icon" target="_blank"><i class="uil uil-linkedin"></i></a>
         <a href="https://github.com/muhammedahmedanwar" className="home__social-icon" target="_blank"><i class="uil uil-github"></i></a>
 
